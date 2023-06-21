5907b369fecba152557c6cd1f1781a5db17ba705
luan <luan@gmail>
2023-06-21 15:04:48 -0300

    criados metodos de post e redirect

--- bootstrap_thymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
+++ bootstrap_thymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
@@ -2,10 +2,14 @@ package br.com.senai.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.senai.model.Produto;
 import br.com.senai.repository.ProdutoRepository;
@@ -22,6 +26,21 @@ public class ProdutoController {
 		List<Produto> produtos = produtoRepository.findAll();
 		model.addAttribute("produtos", produtos);
 		return "produtos";
+	}
+	
+	@GetMapping("/formularioDeCadastro")
+	public String mostrarFormulario(Produto produto) {
+		return "adicionar_produto";
+	}
+	
+	@PostMapping("/adicionarProduto")
+	public String adicionaProduto(@Valid Produto produto
+			, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			return "/adicionar_produto";
+		}
+		produtoRepository.save(produto);
+		return "redirect:/produto";
 	}
 
 }
