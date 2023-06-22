dc70cea7744d7b08a3fddd2c57d59862f3582259
luan <luan@gmail>
2023-06-22 13:18:24 -0300

    criação do formulario de cadastro

--- bootstrap_thymeleaf/src/main/java/br/com/senai/model/Produto.java
+++ bootstrap_thymeleaf/src/main/java/br/com/senai/model/Produto.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Produto {
@@ -12,7 +16,12 @@ public class Produto {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	@NotBlank(message = "nome do produto é obrigatorio")
+	@NotEmpty(message = "nome do produto é obrigatorio")
+	@NotNull(message = "nome do produto é obrigatorio")
+	@Size(min = 5, max = 50, message = "campo deve ter entre 5 e 50 caracteres")
 	private String nomeProduto;
+	
 	private double preco;
 	private int quantidade;
 	private String descricao;
